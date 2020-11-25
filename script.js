a3ec51506e5102835ff5254e40eb38a2cd83a53e
Jonathan R <2140173@gmail.com>
2020-11-26 00:42:41 +0200

    sent message

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ function sendEmail() {
   let name = document.getElementById('name').value;
   let email = document.getElementById('email').value;
   let body = document.getElementById('message').value;
+  let message = 'message sent';
   Email.send({
     SecureToken: 'af057226-e591-4d8b-8d51-8309e688b544',
     To: '‫jsformsubmit@gmail.com‬',
