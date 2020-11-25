64fb8b66d6379f310179081d913e6397c014bfc2
Jonathan R <2140173@gmail.com>
2020-11-26 00:28:48 +0200

    submit email form

---
+++ script.js
@@ -0,0 +1,12 @@
+function sendEmail() {
+  let name = document.getElementById('name').value;
+  let email = document.getElementById('email').value;
+  let body = document.getElementById('message').value;
+  Email.send({
+    SecureToken: 'af057226-e591-4d8b-8d51-8309e688b544',
+    To: '‫jsformsubmit@gmail.com‬',
+    From: email,
+    Subject: 'Email from My web site contact form: ' + name,
+    Body: body,
+  }).then((message) => alert(message));
+}
