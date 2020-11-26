7c380bb7005213b86a626e61468a50a196b0d445
Jonathan R <2140173@gmail.com>
2020-11-26 11:29:42 +0200

    email test

--- script.js
+++ script.js
@@ -1,15 +1,15 @@
 function sendEmail() {
   console.log('start');
-  // let name = document.getElementById('name').value;
-  // let email = document.getElementById('email').value;
-  // let body = document.getElementById('message').value;
-  // let message = 'message sent';
-  // Email.send({
-  //   SecureToken: 'af057226-e591-4d8b-8d51-8309e688b544',
-  //   To: '‫jsformsubmit@gmail.com‬',
-  //   From: email,
-  //   Subject: 'Email from My web site contact form: ' + name,
-  //   Body: body,
-  // }).then((message) => alert(message));
+  let name = document.getElementById('name').value;
+  let email = document.getElementById('email').value;
+  let body = document.getElementById('message').value;
+  let message = 'message sent';
+  Email.send({
+    SecureToken: 'af057226-e591-4d8b-8d51-8309e688b544',
+    To: '‫jsformsubmit@gmail.com‬',
+    From: '‫jsformsubmit@gmail.com‬',
+    Subject: 'Email from My web site contact form: ' + name,
+    Body: email + body,
+  }).then((message) => alert(message));
   console.log('sent');
 }
